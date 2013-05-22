aa3fddb666063bbb0d5eab8d67b741f0f1064298
Effie Arditi <HoneyBook@Effies-MacBook-Pro.local>
2013-05-22 15:08:12 +0300

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogmongo::Application.config.secret_token = '3549e093a038090f99a93ee9e5f0269225ca35d54a99a9ac0ecd24d5d807d51b5666d51f8d6ed852c0b0b0ad85c7ad73cd94822e40d7c759ae4f81950f9731fb'
